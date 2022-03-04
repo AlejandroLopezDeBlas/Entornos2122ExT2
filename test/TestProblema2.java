b792b5d289fdd3c586ec8be3ad35e23b1116da63
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-03-04 12:10:40 +0100

    Elaboración de problema 2

--- test/TestProblema2.java
+++ test/TestProblema2.java
@@ -28,10 +28,45 @@ public class TestProblema2 {
         }
         return cadena;
     }
+    public String devuelveCadena2(){
+        String cadena = "";
+        for (int i = 10; i < 15; i++) {
+            if (i % 3 == 0) {
+                cadena += "Fizz";
+            } else if (i % 5 == 0) {
+                cadena += "Buzz";
+            } else {
+                cadena += i;
+            }
+        }
+        return cadena;
+    }
 
+    private String ProblemaTest2(){
+        String cadena = "";
+        for (int i = 10; i < 15; i++){
+            if (Problema1.esNumeroMultiploDe3(i)) {
+                cadena += "Fizz";
+            } else if (Problema1.esNumeroMultiploDe5(i)) {
+                cadena += "Buzz";
+            } else {
+                cadena += i;
+            }
+        }
+        return cadena;
+    }
     @Test
     public void Test1(){
         if (ProblemaTest1().equalsIgnoreCase(devuelveCadena())){
+            System.out.println("La prueba ha resultado correcta");
+        }
+        else {
+            System.out.println("La prueba ha resultado falsa");
+        }
+    }
+    @Test
+    public void Test2(){
+        if (ProblemaTest2().equalsIgnoreCase(devuelveCadena2())){
             System.out.println("La prueba ha resultado correcta");
         }
         else {
