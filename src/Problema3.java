59c4f7e2fd828b120d6d3e8797abe4ca9ff9fa8f
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-03-04 12:18:41 +0100

    Elaboración de problema 3

---
+++ src/Problema3.java
@@ -0,0 +1,14 @@
+public class Problema3 {
+    public static boolean esNumeroMultiploDe3(Integer n){
+        return n % 3 == 0;
+    }
+    public static boolean esNumeroMultiploDe5(Integer n){
+        return n % 5 == 0;
+    }
+    public static boolean esNumeroMultiploDe7(Integer n){
+        return n % 7 == 0;
+    }
+    public static boolean esNumeroMultiploDe11(Integer n){
+        return n % 11 == 0;
+    }
+}
--- test/TestProblema3.java
+++ test/TestProblema3.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
-public class TestProblema2 {
-    public String devuelveCadena1(){
+public class TestProblema3 {
+    public String devuelveCadena(){
         String cadena = "";
         for (int i = 1; i < 20; i++) {
             if (i % 3 == 0) {
@@ -18,9 +18,9 @@ public class TestProblema2 {
     private String ProblemaTest1(){
         String cadena = "";
         for (int i = 1; i < 20; i++){
-            if (Problema1.esNumeroMultiploDe3(i)) {
+            if (Problema2.esNumeroMultiploDe3(i)) {
                 cadena += "Fizz";
-            } else if (Problema1.esNumeroMultiploDe5(i)) {
+            } else if (Problema2.esNumeroMultiploDe5(i)) {
                 cadena += "Buzz";
             } else {
                 cadena += i;
@@ -41,6 +41,7 @@ public class TestProblema2 {
         }
         return cadena;
     }
+
     private String ProblemaTest2(){
         String cadena = "";
         for (int i = 10; i < 15; i++){
@@ -54,10 +55,43 @@ public class TestProblema2 {
         }
         return cadena;
     }
-
+    public String devuelveCadena3(){
+        String cadena = "";
+        for (int i = 1; i < 20; i++) {
+            if (i % 3 == 0) {
+                cadena += "Fizz";
+            } else if (i % 5 == 0) {
+                cadena += "Buzz";
+            }else if (i % 7 == 0) {
+                cadena += "Foo";
+            }else if (i % 11 == 0) {
+                cadena += "Boo";
+            } else {
+                cadena += i;
+            }
+        }
+        return cadena;
+    }
+    private String ProblemaTest3(){
+        String cadena = "";
+        for (int i = 1; i < 20; i++){
+            if (Problema3.esNumeroMultiploDe3(i)) {
+                cadena += "Fizz";
+            } else if (Problema3.esNumeroMultiploDe5(i)) {
+                cadena += "Buzz";
+            }else if (Problema3.esNumeroMultiploDe7(i)) {
+                cadena += "Foo";
+            }else if (Problema3.esNumeroMultiploDe11(i)) {
+                cadena += "Boo";
+            } else {
+                cadena += i;
+            }
+        }
+        return cadena;
+    }
     @Test
     public void Test1(){
-        if (ProblemaTest1().equalsIgnoreCase(devuelveCadena1())){
+        if (ProblemaTest1().equalsIgnoreCase(devuelveCadena())){
             System.out.println("La prueba ha resultado correcta");
         }
         else {
@@ -74,4 +108,14 @@ public class TestProblema2 {
             System.out.println("La prueba ha resultado falsa");
         }
     }
+    @Test
+    public void Test3(){
+        if (ProblemaTest3().equalsIgnoreCase(devuelveCadena3())){
+            System.out.println("La prueba ha resultado correcta");
+        }
+        else {
+            System.out.println("La prueba ha resultado falsa");
+        }
+    }
+    
 }
